002862be2696b47c939416413a0029bf52d43856
vansieu2502 <nguyenvansieu2502@gmail.com>
2024-04-23 01:10:34 +0700

    bo sung DB

--- example-app/database/migrations/2024_04_21_155200_create_user_table.php
+++ example-app/database/migrations/2024_04_21_155200_create_user_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name', 100);
             $table->string('email', 100)->unique();
             $table->string('password', 100);
+            $table->enum('role', ['khachhang', 'quanly'])->default('khachhang');
             $table->timestamps();
         });
     }

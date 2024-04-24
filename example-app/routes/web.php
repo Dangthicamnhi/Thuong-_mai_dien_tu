faf2069c5502f6fa6a84117cbd4fa514a9ceae21
nam <lehoainam7704@gmail.com>
2024-04-24 19:24:19 +0700

    Resolved merge conflict

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,17 +2,8 @@
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\login;
-=======
-<<<<<<< Updated upstream
-
-=======
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\LoginController;
->>>>>>> Stashed changes
->>>>>>> login
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,23 +18,8 @@ use App\Http\Controllers\LoginController;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-Route::get('/login', [login::class, 'showLoginForm'])->name('login');
-=======
-<<<<<<< Updated upstream
-Route::get('/login', function () {
-    return view('login');
-});
-=======
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
->>>>>>> login
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
-
-
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> login
 
